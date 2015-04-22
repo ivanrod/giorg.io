24ee2a2306638e42750cef191ed481bac6c1f6f6
Ivan Rodriguez <frivanrodriguez@gmail.com>
2015-04-22 14:10:17 +0200

    Added list to mycourses

--- src/app/dashboard/dashboard.controller.js
+++ src/app/dashboard/dashboard.controller.js
@@ -1,4 +1,25 @@
 function DashboardCtrl(sharedData){
 	'use strict';
 	var vm = this;
+	vm.coursera = [
+		{
+		title: "Curso #01", 
+		description: "descripcion Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, deleniti, velit iusto quo tempore repellat explicabo distinctio optio sapiente at consequatur dolores, quidem, ea voluptatem reprehenderit facilis itaque est voluptatum!"
+		},
+		{
+		title: "Curso #05", 
+		description: "descripcion Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, deleniti, velit iusto quo tempore repellat explicabo distinctio optio sapiente at consequatur dolores, quidem, ea voluptatem reprehenderit facilis itaque est voluptatum!"
+		}
+	];
+
+	vm.edx = [
+		{
+		title: "Curso #29", 
+		description: "descripcion Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur esse doloremque fuga quis, in quo amet ipsam harum vel doloribus asperiores architecto praesentium aliquam unde quisquam, obcaecati ullam explicabo, quos."
+		},
+		{
+		title: "Curso #79", 
+		description: "descripcion Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur esse doloremque fuga quis, in quo amet ipsam harum vel doloribus asperiores architecto praesentium aliquam unde quisquam, obcaecati ullam explicabo, quos."
+		}
+	];
 }
